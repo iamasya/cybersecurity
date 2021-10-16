a5a7e1c2fc22838302d7bcbf4e545e860c700c69
Anastasiia Radyvonenko <a.radyvonenko@gmail.com>
2021-10-16 20:13:42 +0300

    lab1

---
+++ pz1/pz1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace pz1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random(1235);
+            random.Next(1, 10);
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(random.Next(1, 10));
+            }
+        }
+    }
+}
---
+++ pz1/pz1_1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace pz1_1
+{
+    class Program
+    {
+        public static byte[] GenerateRandomNumber(int length)
+        {
+            using (var rndNumberGenerator = new RNGCryptoServiceProvider())
+            {
+                var randomNumber = new byte[length];
+                rndNumberGenerator.GetBytes(randomNumber);
+                return randomNumber;
+            }
+        }
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                string randomNumber = Convert.ToBase64String(GenerateRandomNumber(32));                
+                Console.WriteLine(randomNumber);
+                
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
