7a42f2a6f1a5288385baf35d9fdeb296e3e31d9c
Anastasiia Radyvonenko <a.radyvonenko@gmail.com>
2021-11-21 20:53:50 +0200

    lab5

---
+++ lab5.4/lab5.4/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Diagnostics;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace lab5._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string passwordToHash = "mit-21";
+            HashPassword(passwordToHash, 220000); //перше значення = номер варіанта * 10'000; мій номер у списку: 22
+            HashPassword(passwordToHash, 270000); //крок = 50'000
+            HashPassword(passwordToHash, 320000);
+            HashPassword(passwordToHash, 370000);
+            HashPassword(passwordToHash, 420000);
+            HashPassword(passwordToHash, 470000);
+            HashPassword(passwordToHash, 520000);
+            HashPassword(passwordToHash, 570000);
+            HashPassword(passwordToHash, 620000);
+            HashPassword(passwordToHash, 670000);
+            Console.ReadLine();
+        }
+
+        private static void HashPassword(string passwordToHash, int numberOfRounds)
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+
+            var hashedPasswordmd5 = PBKDF2.HashPasswordMD5(Encoding.UTF8.GetBytes(passwordToHash), PBKDF2.GenerateSalt(), numberOfRounds);
+            var hashedPasswordsha1 = PBKDF2.HashPasswordSHA1(Encoding.UTF8.GetBytes(passwordToHash), PBKDF2.GenerateSalt(), numberOfRounds);
+            var hashedPasswordsha256 = PBKDF2.HashPasswordSHA256(Encoding.UTF8.GetBytes(passwordToHash), PBKDF2.GenerateSalt(), numberOfRounds);
+            var hashedPasswordsha384 = PBKDF2.HashPasswordSHA384(Encoding.UTF8.GetBytes(passwordToHash), PBKDF2.GenerateSalt(), numberOfRounds);
+            var hashedPasswordsha512 = PBKDF2.HashPasswordSHA512(Encoding.UTF8.GetBytes(passwordToHash), PBKDF2.GenerateSalt(), numberOfRounds);
+
+            sw.Stop();
+            Console.WriteLine();
+            Console.WriteLine("Password to hash : " + passwordToHash);
+            Console.WriteLine("MD5 Hashed Password : " + Convert.ToBase64String(hashedPasswordmd5));
+            Console.WriteLine("SHA1 Hashed Password : " + Convert.ToBase64String(hashedPasswordsha1));
+            Console.WriteLine("SHA256 Hashed Password : " + Convert.ToBase64String(hashedPasswordsha256));
+            Console.WriteLine("SHA384 Hashed Password : " + Convert.ToBase64String(hashedPasswordsha384));
+            Console.WriteLine("SHA512 Hashed Password : " + Convert.ToBase64String(hashedPasswordsha512));
+            Console.WriteLine("Iterations <" + numberOfRounds + ">  Elapsed Time: " + sw.ElapsedMilliseconds + "ms");
+
+        }
+    }
+
+    public class PBKDF2
+    {
+        public static byte[] GenerateSalt()
+        {
+            using (var randomNumberGenerator = new RNGCryptoServiceProvider())
+            {
+                var randomNumber = new byte[32];
+                randomNumberGenerator.GetBytes(randomNumber);
+                return randomNumber;
+            }
+        }
+
+        public static byte[] HashPasswordMD5(byte[] toBeHashed, byte[] salt, int numberOfRounds)
+        {
+            using (var rfc2898 = new Rfc2898DeriveBytes(toBeHashed, salt, numberOfRounds, HashAlgorithmName.MD5))
+            {
+                return rfc2898.GetBytes(16);//Коли GetBytes викликається для вилучення
+                                            //хеш, ми отримуємо лише 16 байт; це тому, що базова функція хешування
+                                            //всередині Rfc2898DeriveBytes використовує алгоритм MD-5 для створення хешу. MD - 5
+                                            //створює 128 - бітний або 16 - байтний хеш - код.
+            }
+        }
+
+        public static byte[] HashPasswordSHA1(byte[] toBeHashed, byte[] salt, int numberOfRounds)
+        {
+            using (var rfc2898 = new Rfc2898DeriveBytes(toBeHashed, salt, numberOfRounds, HashAlgorithmName.SHA1))
+            {
+                return rfc2898.GetBytes(20); //SHA - 1 створює 160 - бітний або 20 - байтний хеш - код.
+            }
+        }
+
+        public static byte[] HashPasswordSHA256(byte[] toBeHashed, byte[] salt, int numberOfRounds)
+        {
+            using (var rfc2898 = new Rfc2898DeriveBytes(toBeHashed, salt, numberOfRounds, HashAlgorithmName.SHA256))
+            {
+                return rfc2898.GetBytes(32);//SHA - 256 створює 256 - бітний або 32 - байтний хеш - код.
+            }
+        }
+
+        public static byte[] HashPasswordSHA384(byte[] toBeHashed, byte[] salt, int numberOfRounds)
+        {
+            using (var rfc2898 = new Rfc2898DeriveBytes(toBeHashed, salt, numberOfRounds, HashAlgorithmName.SHA384))
+            {
+                return rfc2898.GetBytes(48);//SHA - 384 створює 384 - бітний або 48 - байтний хеш - код.
+            }
+        }
+
+        public static byte[] HashPasswordSHA512(byte[] toBeHashed, byte[] salt, int numberOfRounds)
+        {
+            using (var rfc2898 = new Rfc2898DeriveBytes(toBeHashed, salt, numberOfRounds, HashAlgorithmName.SHA512))
+            {
+                return rfc2898.GetBytes(64);//SHA - 512 створює 512 - бітний або 64 - байтний хеш - код.
+            }
+        }
+    }
+}
---
+++ lab5.5/lab5.5/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace lab5._5
+{
+    class Program
+    {
+        static byte[] ComputeHashSHA256(byte[] toBeHashed)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                return sha256.ComputeHash(toBeHashed);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            byte[] Salt = PBKDF2.GenerateSalt();
+            Console.WriteLine("----------------------------------------------------------");
+            Console.WriteLine("Register to our website by creating your login & password!");
+            Console.WriteLine("----------------------------------------------------------");
+            Console.WriteLine("");
+            Console.WriteLine("Create your login name ->");
+            string login = Console.ReadLine();
+            Console.WriteLine("Create your password ->");
+            string password = Console.ReadLine();
+
+            Console.WriteLine("Congrats! Your personal account has been created! ");
+
+            var sha256Forstrl = Convert.ToBase64String(ComputeHashSHA256(Encoding.Unicode.GetBytes(login))); 
+            var sha256Forstrp = Convert.ToBase64String(PBKDF2.HashPassword(Encoding.Unicode.GetBytes(Convert.ToString(password)), Salt, 220000));//число ітерацій = номер варіанта * 10'000.
+                                                                                                                                                 //мій варіант (номер у списку): 22
+
+            int tt = 0; //кількість спроб
+            string loginHash, passwordHash;
+            Console.WriteLine("");
+            Console.WriteLine("---------------------------------------------------------");
+            Console.WriteLine("Enter to our website by using your login & password! ");
+            Console.WriteLine("---------------------------------------------------------");
+            Console.WriteLine("");
+            Console.WriteLine("WARNING! You have only 3 attempts to log into your account!");
+            Console.WriteLine("");
+
+            do
+            {
+                Console.WriteLine("Please enter your login -> ");
+                login = Console.ReadLine();
+                loginHash = Convert.ToBase64String(ComputeHashSHA256(Encoding.Unicode.GetBytes(login)));
+                Console.WriteLine("Please enter your password -> ");
+                password = Console.ReadLine();
+                passwordHash = Convert.ToBase64String(PBKDF2.HashPassword(Encoding.Unicode.GetBytes(Convert.ToString(password)), Salt, 220000));
+
+                if (loginHash != sha256Forstrl || passwordHash != sha256Forstrp) //якщо логін або пароль не співпадають, надається ще одна спроба
+                {
+                    tt++;
+                }
+                else
+                {
+                    tt = 1;
+                }
+            }
+            while ((loginHash != sha256Forstrl || passwordHash != sha256Forstrp) && (tt != 3)); ;//"do" буде виконуватися допоки не буде все правильно виведене
+                                                                                                 //або допоки логін або пароль не співпадають та кількість спроб не = 3
+
+            if (tt == 3)
+            {
+                Console.Write("\nPlease check that your input is correct...Try again!\n\n");
+            }
+            else
+            {
+                Console.Write("\nYou successfully entered to your personal account!\n\n");
+            }
+            Console.ReadLine();
+        }
+    }
+
+    public class PBKDF2
+    {
+        public static byte[] GenerateSalt()
+        {
+            using (var randomNumberGenerator = new RNGCryptoServiceProvider())
+            {
+                var randomNumber = new byte[32];
+                randomNumberGenerator.GetBytes(randomNumber);
+                return randomNumber;
+            }
+        }
+        public static byte[] HashPassword(byte[] toBeHashed, byte[] salt, int numberOfRounds)
+        {
+            using (var rfc2898 = new Rfc2898DeriveBytes(toBeHashed, salt, numberOfRounds))
+            {
+                return rfc2898.GetBytes(20);
+            }
+        }
+    }
+}
